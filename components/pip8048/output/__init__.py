8388981bb8021f7fdb96e5981b5730eba3bb04fe
Dave Prue <dave@prue.com>
2026-05-04 16:06:45 +0800

    Update __init__.py

--- components/pip8048/output/__init__.py
+++ components/pip8048/output/__init__.py
@@ -43,6 +43,8 @@ CONF_BATTERY_REDISCHARGE_VOLTAGE = "battery_redischarge_voltage"
 CONF_INPUT_VOLTAGE_RANGE = "input_voltage_range"
 CONF_ENABLE_RESTART_ON_OVERLOAD = "enable_restart_on_overload"
 CONF_DISABLE_RESTART_ON_OVERLOAD = "disable_restart_on_overload"
+CONF_ENABLE_MENU_RETURNS_HOME = "enable_menu_returns_home"
+CONF_DISABLE_MENU_RETURNS_HOME = "disable_menu_returns_home"
 
 TYPES = {
     CONF_BATTERY_BULK_VOLTAGE: (
@@ -116,6 +118,12 @@ TYPES = {
     CONF_DISABLE_RESTART_ON_OVERLOAD: (
         [0, 1],
         "PDu"),
+    CONF_ENABLE_MENU_RETURNS_HOME: (
+        [0, 1],
+        "PEk"),
+    CONF_DISABLE_MENU_RETURNS_HOME: (
+        [0, 1],
+        "PDk"),
 }
 
 CONFIG_SCHEMA = PIPSOLAR_COMPONENT_SCHEMA.extend(
